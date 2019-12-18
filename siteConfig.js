96dc39246ea6a1ebdd41c53f89f78aa136a391cd
leils <e.leiachang@gmail.com>
2019-12-17 22:25:28 -0800

    fixing broken links and scripts

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,7 @@
 module.exports = {
   name: "Leia",
   shortName: "Leia",
-  description:
-    "Programmer, artist, photographer, jack-of-all-trades with a focus on New Media and UX.",
+  description: "Programmer, artist, jack-of-all-trades.",
   url: "https://leiac.me", // Domain of your site without prefix!
   prefix: "/",
   author: "Leia Chang",

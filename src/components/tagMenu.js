d0907baba123b1c65bdc52dc157166af46313b1f
Leia <e.leiachang@gmail.com>
2021-09-08 19:02:30 -0400

    tag nav for mobile

--- src/components/tagMenu.js
+++ src/components/tagMenu.js
@@ -7,37 +7,19 @@ const TagMenu = props => {
     typeof window !== "undefined" ? window.location.pathname.split("/")[1] : ""
 
   return (
-    <div className="site-head-container">
-      <a
-        className="nav-burger"
-        href={`#`}
-        onClick={() => setToggleNav(!toggleNav)}
-      >
-        <div
-          className="hamburger hamburger--collapse"
-          aria-label="Menu"
-          role="button"
-          aria-controls="navigation"
-        >
-          <div className="hamburger-box">
-            <div className="hamburger-inner" />
-          </div>
-        </div>
-      </a>
-      <nav id="swup" className="site-head-left">
-        <ul className="nav" role="menu">
-          <li className={`nav-home`} role="menuitem">
-            <Link to={`/blog`}>All Posts</Link>
-          </li>
-          <li className={`nav-home`} role="menuitem">
-            <Link to={`/tags/itp`}>ITP</Link>
-          </li>
-          <li className={`nav-home`} role="menuitem">
-            <Link to={`/tags`}>All Tags</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="tag-menu">
+      <ul role="menu">
+        <li role="menuitem">
+          <Link to={`/blog`}>All Posts</Link>
+        </li>
+        <li role="menuitem">
+          <Link to={`/tags/itp`}>ITP</Link>
+        </li>
+        <li role="menuitem">
+          <Link to={`/tags`}>All Tags</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 

4144250fee5b48f3f06d07dd9a134b0347a036dc
Leia <e.leiachang@gmail.com>
2021-10-20 10:00:23 -0400

    remove offline for better caching behaviour

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,7 +131,7 @@ module.exports = {
         icon: `content/assets/icon.png`,
       },
     },
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     `gatsby-plugin-react-helmet`,
   ],
 }
